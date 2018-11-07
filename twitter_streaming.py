744341ceae89ddbcae143fd6e245e781fc9f0d74
Juan Antonio <ruchin10@gmail.com>
2018-11-07 19:34:43 +0100

    Added logging

--- twitter_streaming.py
+++ twitter_streaming.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 import tweepy
 import json
 import os
+import logging
 from pymongo import MongoClient
 
 MONGO_HOST = 'mongodb+srv://' + os.environ['MONGO_USER'] + ':' + os.environ['MONGO_PASS'] + '@cluster0-2bfcj.mongodb.net/test?retryWrites=true'
@@ -13,16 +14,18 @@ CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
 ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
 ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
  
+logging.info("Starting app")
+
 class StreamListener(tweepy.StreamListener):    
     #This is a class provided by tweepy to access the Twitter Streaming API. 
  
     def on_connect(self):
         # Called initially to connect to the Streaming API
-        print("You are now connected to the streaming API.")
+        logging.info("You are now connected to the streaming API.")
  
     def on_error(self, status_code):
         # On error - if an error occurs, display the error / status code
-        print('An Error has occured: ' + repr(status_code))
+        logging.error('An Error has occured: ' + repr(status_code))
         return False
  
     def on_data(self, data):
@@ -40,7 +43,7 @@ class StreamListener(tweepy.StreamListener):
             created_at = datajson['created_at']
  
             #print out a message to the screen that we have collected a tweet
-            print("Tweet collected at " + str(created_at))
+            logging.debug("Tweet collected at " + str(created_at))
             
             #insert the data into the mongoDB into a collection called tweets
             #if tweets doesn't exist, it will be created.
@@ -54,6 +57,6 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 listener = StreamListener(api=tweepy.API(wait_on_rate_limit=True)) 
 streamerSearch = tweepy.Stream(auth=auth, listener=listener)
 streamerOfficial = tweepy.Stream(auth=auth, listener=listener)
-print("Tracking: " + str(WORDS))
+logging.info("Tracking: " + str(WORDS))
 streamerSearch.filter(track=WORDS)
 streamerOfficial.filter(follow=os.environ['OFFICIAL_METRO_ACCOUNT'])
