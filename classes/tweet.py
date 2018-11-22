01bf532e3bcd6d2ac5a625b2b24cf2f9bec02036
Juan Antonio <ruchin10@gmail.com>
2018-11-23 00:09:44 +0100

    Created tweet class

---
+++ classes/tweet.py
@@ -0,0 +1,49 @@
+import nltk
+import json
+from database import Database
+
+class Tweet:
+
+    SPANISH = "es"
+    ENGLISH = "en"
+
+    def __init__(self, jsonData=None):
+        self.processJson(jsonData)
+        self.generateWords()
+
+    def processJson(self, jsonData):
+        self.jsonData = json.loads(jsonData)
+
+        self.text = self.extractText()
+
+        self.text = self.jsonData['extended_texttext']
+        self.created_at = self.jsonData['created_at']
+        self.user_id = self.jsonData['user']['id']
+        self.lang = self.jsonData['lang']
+
+    def extractText(self):
+        text = ""
+        if self.jsonData.has_key('extended_tweet'):
+            text = self.jsonData['extended_tweet']['full_text']
+        else:
+            text = self.jsonData['text']
+
+        return text
+
+    def generateWords(self):
+        tokens = [word for word in nltk.word_tokenize(self.text) if word.isalpha()]
+        stopwords = nltk.corpus.stopwords.words("spanish")
+        self.words = list(set(tokens) - set(stopwords))
+
+    def getWords(self):
+        return self.words
+
+    def saveTweet(self):
+        database = Database()
+        database.insert(collection_name = "tweets", data=self.jsonData)
+    
+    def isSpanish(self):
+        return self.lang == Tweet.SPANISH
+    
+    def isEnglish(self):
+        return self.lang == Tweet.ENGLISH
